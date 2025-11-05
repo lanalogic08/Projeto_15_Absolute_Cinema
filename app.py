0e9822d0e83b6db1b209b836e20c59bf5d5e5323
Allana Almeida <aparecidaallana09@gmail.com>
2025-11-05 15:33:09 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -9,19 +9,27 @@ st.set_page_config(
 )
 
 # Carregar dados dos filmes
-
-
+filmes  = pd.read_csv('filmes.csv')
 # Sidebar apenas com filtro de gênero
-# coloque a logo e a selectbox
+st.sidebar.title('ToroFlix')
+st.sidebar.image('logo.png')
+st.sidebar.title("Filtros")
+genero_escolhido = st.sidebar.selectbox(
+    "Filtrar por gênero:", 
+    ["Todos"] + sorted(filmes['Genre'].unique().tolist())
 
+)
 
 # Aplicar filtro
-
+if genero_escolhido == "Todos":
+    filmes_filtrados = filmes 
+else:
+    filmes_filtrados = filmes[filmes['Genre']==genero_escolhido]
 
 # Título da página
-
+st.title("Top  filmes IMDB")
 # Mostrar quantidade de filmes
-
+st.write(f"**{len(filmes_filtrados)}filmes filtrados**")
 
 # Mostrar filmes em grid (3 colunas)
 colunas = st.columns(3)
@@ -37,7 +45,11 @@ for index, filme in filmes_filtrados.iterrows():
             st.image(filme['Image URL'], use_container_width=True)
             
             # Informações do filme
-
+            st.markdown(f"# 🎬 {filme['Title']} ({filme['Year']})")
+            st.markdown(f"### ⏱️ **Duração: {filme['Duration']}**")
+            st.markdown(f"### 🎭 **Gênero: {filme['Genre']}**")
+            st.markdown(f"### 🏆 **Posição no Ranking: #{filme['Rank']}**")
+                
             
             # Botão para ver no IMDb
             if st.button(f"Ver no IMDb", key=f"btn_{filme['Rank']}"):
